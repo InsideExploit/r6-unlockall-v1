8c17de9517d9f88043cb658c7795bab178f3fa52
InsideExploit <71732299+InsideExploit@users.noreply.github.com>
2020-09-23 17:57:52 +0200

    Added json file config configuration.

--- Unlock.cpp
+++ Unlock.cpp
@@ -4,10 +4,61 @@
 #include "Memory.h"
 #include "Config.h"
 
+#include <fstream>
+#include <filesystem>
+#include <direct.h>
+
+#include "Json.h"
+using namespace nlohmann;
+
 Memory* memory;
+
+std::string CurrentPath()
+{
+    char* cwd = _getcwd(0, 0);
+    std::string directory(cwd);
+    std::free(cwd);
+    return directory;
+}
 
 int main()
 {
+    json js;
+
+    std::string configPath = CurrentPath() + "\\config.json";
+
+    std::ifstream configR(configPath);
+
+    if (!configR)
+    {
+
+        std::cout << "[LOADING] Creating the json config file because is missing...\n";
+
+        Sleep(300);
+
+        std::ofstream configW;
+        configW.open(configPath);
+        configW << "{\n";
+        configW << "  \"Offset\": 59854116,\n";
+        configW << "  \"Enable\": 112,\n";
+        configW << "  \"Disable\": 113,\n";
+        configW << "  \"Normal\": 114\n";
+        configW << "}";
+
+        configW.close();
+
+        Sleep(300);
+
+        std::cout << "[SUCCESS] The json config file got created... please restart application.\n";
+    }
+
+    configR >> js;
+    
+    uintptr_t offsConfig = js["Offset"];
+
+    uintptr_t hotkeyEnable = js["Enable"];
+    uintptr_t hotkeyDisable = js["Disable"];
+    uintptr_t hotkeyNormal = js["Normal"];
 
     SetConsoleTitleA("R6 Unlock All | Shadow Legacy | InsideExploit[UC]");
 
@@ -36,31 +87,35 @@ int main()
         exit(-3);
     }
 
-    std::cout << "[WARNING] Please use an command.\n\n";
-    std::cout << "[INFO] F1: Unlock All.\n";
-    std::cout << "[INFO] F2: Disable All.\n";
-    std::cout << "[INFO] F3: Normal All.\n";
+    std::cout << "[INFO] Please use an command.\n\n";
+    std::cout << "[HOTKEY] F1: Unlock All.\n";
+    std::cout << "[HOTKEY] F2: Disable All.\n";
+    std::cout << "[HOTKEY] F3: Normal All.\n";
+
+    std::cout << "\n[IMPORTANT] Remember, that one showed key are the default one.\n";
+
 
     std::cout << "" << std::endl;
 
     while (1)
     {
-        if (GetAsyncKeyState(VK_F1) & 0x1)
+        if (GetAsyncKeyState(hotkeyEnable) & 0x1)
         {
             std::cout << "[SUCCESS] Everything was unlocked. [5.0]\n";
-            WriteProcessMemory(hProcess, (LPVOID)(base + Config::Offsets::unlock), Config::Offsets::BytepatchEnable, sizeof(Config::Offsets::BytepatchEnable), NULL);
+            WriteProcessMemory(hProcess, (LPVOID)(base + offsConfig), Config::Offsets::BytepatchEnable, sizeof(Config::Offsets::BytepatchEnable), NULL);
         }
 
-        if (GetAsyncKeyState(VK_F2) & 0x1)
+        if (GetAsyncKeyState(hotkeyDisable) & 0x1)
         {
             std::cout << "[SUCCESS] Everything was blocked.  [5.1]\n";
-            WriteProcessMemory(hProcess, (LPVOID)(base + Config::Offsets::unlock), Config::Offsets::BytepatchDisable, sizeof(Config::Offsets::BytepatchDisable), NULL);
+            WriteProcessMemory(hProcess, (LPVOID)(base + offsConfig), Config::Offsets::BytepatchDisable, sizeof(Config::Offsets::BytepatchDisable), NULL);
         }
 
-        if (GetAsyncKeyState(VK_F3) & 0x1)
+        if (GetAsyncKeyState(hotkeyNormal) & 0x1)
         {
             std::cout << "[SUCCESS] Everything was resetted. [5.2]\n";
-            WriteProcessMemory(hProcess, (LPVOID)(base + Config::Offsets::unlock), Config::Offsets::BytepatchNormal, sizeof(Config::Offsets::BytepatchNormal), NULL);
+            WriteProcessMemory(hProcess, (LPVOID)(base + offsConfig), Config::Offsets::BytepatchNormal, sizeof(Config::Offsets::BytepatchNormal), NULL);
         }
     }
+    
 }
